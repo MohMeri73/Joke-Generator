ff428ef034562ff001b63cc2c3e424a2b156970b
MohammadReza <mohammadreza.jafari.hp@gmail.com>
2023-03-11 23:12:16 +0330

    add font & create app.js & complete style

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
-let jokeSection = document.getElementById("joke-section");
-let generateBtn = document.getElementById("btn");
-const API =
+const loader = document.querySelector("#loading");
+
+function displayLoading() {
+  loader.classList.add("display");
+  loader.style.display = "block";
+
+  // // to stop loading after some time
+  // setTimeout(() => {
+  //     loader.classList.remove("display");
+  // }, 1000);
+}
+function hideLoading() {
+  loader.classList.remove("display");
+  loader.style.display = "none";
+}
+
+const jokeContainer = document.getElementById("joke-section");
+const btn = document.getElementById("btn");
+const url =
   "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
 
 let getJoke = () => {
-  jokeSection.classList.remove("fade");
-  fetch(API)
+  displayLoading();
+  jokeContainer.classList.remove("fade");
+  fetch(url)
     .then((data) => data.json())
     .then((item) => {
-      jokeSection.textContent = `${item.joke}`;
-      jokeSection.classList.add("fade");
+      jokeContainer.textContent = `${item.joke}`;
+      jokeContainer.classList.add("fade");
+      hideLoading();
     });
 };
 btn.addEventListener("click", getJoke);
-
-// const fetchJoke = async () => {
-//   let response = await fetch("https://api.chucknorris.io/jokes/random");
-//   let data = await response.json();
-//   document.getElementById("joke-section").innerText = data.value;
-// };
